204292b05946e0a5aa804b02698ce810f8dad2ec
Alicia Mara <alicia@Alicias-Laptop.local>
2024-03-28 22:55:31 -0700

    Add gunicorn and requirements.txt

--- projects.py
+++ projects.py
@@ -1,10 +1,10 @@
 projects = [
     {
         "image": "",
-        "name": "",
-        "techs": [],
-        "description": """""",
-        "live": "",
-        "repo": ""
+        "name": "Number Guessing Game",
+        "techs": ["Python"],
+        "description": "A console number guessing game that prompts a player to choose a number and tells them higher or lower until they choose correctly.",
+        "live": "https://replit.com/@aliciama16/guess-a-number-1?v=1",
+        "repo": "https://github.com/aliciama16/guess-a-number"
     }
 ]
