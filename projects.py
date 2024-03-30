eebd8f63b9294579f04c8a4fbf52223f0070a6c0
Alicia M <aliciama16@gmail.com>
2024-03-29 23:19:13 -0700

    Update projects.py

--- projects.py
+++ projects.py
@@ -7,4 +7,20 @@ projects = [
         "live": "https://replit.com/@aliciama16/guess-a-number-1?v=1",
         "repo": "https://github.com/aliciama16/guess-a-number"
     }
+    {
+        "image": "",
+        "name": "A Python Quiz Game",
+        "techs": ["Python"],
+        "description": "A Python quiz game that tests your knowledge about my favorite composer, Rachmaninoff. The quiz is multiple choice and displays your score at the end.",
+        "live": "https://replit.com/@aliciama16/rachmaninoff-quiz-game?v=1 ",
+        "repo": "https://github.com/aliciama16/rachmaninoff-quiz-game"
+    }
+    {
+        "image": "",
+        "name": "What's the Weather?",
+        "techs": ["Python"],["API"]
+        "description": "A website that asks the user to input a city name. An API retrieves and displays the weather.",
+        "live": "https://aliciama16.github.io/whatstheweather/",
+        "repo": "https://github.com/aliciama16/whatstheweather"
+    }    
 ]
